b7622c48c4ce99e06888b02fcb3a1c163614d11e
GeorgyShv <mr.gosha9@mail.ru>
2022-11-19 10:22:40 +0300

    class Card added

--- cow006.py
+++ cow006.py
@@ -1,3 +1,24 @@
+class Card:
+    def __init__(self, num, score):
+        self.num = num
+        self.score = score
+
+
+    def __repr__(self):
+        # print('call __repr__')
+        return f'{self.num}, {self.score}'
+
+    def __str__(self):
+        return tr(card))
+
+    # сравнивает 2 карты по номиналу
+
+
+# класс закончился
+card = Card(25, 2)
+print(card)        # str(card) --> card.__str__
+print(repr(card))  # repr(card) --> card.__repr__
+
 class CardList:
     def __init__(self):
 
